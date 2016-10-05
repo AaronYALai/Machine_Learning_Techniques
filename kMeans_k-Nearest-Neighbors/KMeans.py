c383d387c023485536b25667fef290fb2bed2da1
AaronLai <aaronlaiya@icloud.com>
2016-10-05 16:36:59 +0800

    kNN and KMeans

--- kMeans_k-Nearest-Neighbors/KMeans.py
+++ kMeans_k-Nearest-Neighbors/KMeans.py
@@ -2,71 +2,89 @@ import numpy as np
 import pandas as pd
 import time
 
+
 class KMeans(object):
     """K-Means clustering algorithm"""
-    def __init__(self,k):
+    def __init__(self, k):
         self.k = k
-        
-    def cluster(self,X):
+
+    def cluster(self, X):
         """Perform KMeans clustering to find the centers of clusters"""
-        #Initialization
-        centers = np.array([X[ind] for ind in np.random.choice(len(X),self.k)])
-        clusters = [0]*(len(X))
-        
+        # Initialization
+        random_indexes = np.random.choice(len(X), self.k)
+        centers = np.array([X[ind] for ind in random_indexes])
+        clusters = [0] * (len(X))
+
         conti = False
         while True:
-            #Find optimal partition of the data points given the centers
-            for ix,x in enumerate(X):
+            # Find optimal partition of the data points given the centers
+            for ix, x in enumerate(X):
                 best = float("inf")
-                for ind,center in enumerate(centers):
-                    dist = np.sum((x-center)**2)
+
+                for ind, center in enumerate(centers):
+                    dist = np.sum((x - center)**2)
                     if dist < best:
                         best = dist
                         clus = ind
+
                 if clus != clusters[ix]:
                     clusters[ix] = clus
                     conti = True
-            
-            #Re-compute the optimal centers given the partition
-            add = {}        
+
+            # Re-compute the optimal centers given the partition
+            add = {}
             for k in range(self.k):
-                 add[k] = np.zeros(len(X[0]))
-            num = [0]*self.k
-            for ix,ind in enumerate(clusters):
+                add[k] = np.zeros(len(X[0]))
+
+            num = [0] * self.k
+            for ix, ind in enumerate(clusters):
                 add[ind] += X[ix]
                 num[ind] += 1
-            centers = np.array([add[j]/num[j] for j in range(self.k)])
-            #stop if the partition hasn't changed
+
+            centers = np.array([add[j] / num[j] for j in range(self.k)])
+            # stop if the partition hasn't changed
             if conti:
                 conti = False
             else:
                 break
-                
+
         self.centers = centers
         self.clusters = clusters
         self.data = X
+
         return centers, clusters
-        
-    def square_error(self,Print=False):
+
+    def square_error(self, Print=False):
         """Compute average square error to centers"""
         Error = 0
         for i in range(len(self.data)):
-            Error += np.sum((self.data[i]-self.centers[self.clusters[i]])**2)
-        Error = Error/len(self.data)
+            Error += np.sum((self.data[i] - self.centers[self.clusters[i]])**2)
+
+        Error = Error / len(self.data)
         if Print:
-            print("Average square error to centers: %.4f"%Error)
+            print("Average square error to centers: %.4f" % Error)
+
         return Error
 
-Data = pd.read_csv('Data/hw4_kmeans_train.dat',header=None,delim_whitespace=True)
-print('Data loaded\n',Data.head())
 
-print('Start clustering...')
-start = time.clock()
-Ein = []
-for i in range(500):
-    K = KMeans(10)
-    centers, clusters = K.cluster(Data.values)
-    Ein.append(K.square_error())
-    
-print('Averaged Ein: %.4f, using %.2f seconds.'%(np.mean(Ein),time.clock()-start))
-print('Centers: ',centers)
+def main():
+    Data = pd.read_csv('Data/hw4_kmeans_train.dat', header=None,
+                       delim_whitespace=True)
+    print('Data loaded\n', Data.head())
+
+    print('\nStart clustering...')
+    start = time.clock()
+    Ein = []
+
+    for i in range(500):
+        K = KMeans(10)
+        centers, clusters = K.cluster(Data.values)
+        Ein.append(K.square_error())
+
+    print('\tAveraged Ein: %.4f, using %.2f seconds.' %
+          (np.mean(Ein), time.clock() - start))
+    print('\nCenters:\n', centers)
+
+
+if __name__ == '__main__':
+    main()
--- kMeans_k-Nearest-Neighbors/k_Nearest_Neighbor.py
+++ kMeans_k-Nearest-Neighbors/k_Nearest_Neighbor.py
@@ -2,45 +2,59 @@ import numpy as np
 import pandas as pd
 import time
 
+
 class kNN(object):
     """Uniform voting by k nearest neighbors as predictions"""
-    def __init__(self,k):
+    def __init__(self, k):
         self.k = k
-        
-    def train(self,X,y):
+
+    def train(self, X, y):
         self.Xtrain = X
         self.ytrain = y
-        
-    def predict(self,Xtest):
+
+    def predict(self, Xtest):
         """Find the k nearest neighbor of the input and do uniform voting"""
         prediction = []
         for xtest in Xtest:
-            Ind_Dist = [(float("inf"),0)]*self.k  #initialize k nearest neighbors
-            for ind,xtrain in enumerate(self.Xtrain):
-                dist = np.sum((xtest-xtrain)**2)
+            # initialize k nearest neighbors
+            Ind_Dist = [(float("inf"), 0)] * self.k
+
+            for ind, xtrain in enumerate(self.Xtrain):
+                dist = np.sum((xtest - xtrain)**2)
+
                 if dist <= Ind_Dist[-1][0]:
-                    Ind_Dist[-1] = (dist,ind)
+                    Ind_Dist[-1] = (dist, ind)
                     Ind_Dist.sort()
-            predict = 1 if sum([self.ytrain[ind] for _,ind in Ind_Dist]) >= 0 else -1
+
+            voted_result = sum([self.ytrain[ind] for _, ind in Ind_Dist])
+            predict = 1 if voted_result >= 0 else -1
             prediction.append(predict)
+
         return np.array(prediction)
 
-Train = pd.read_csv('Data/hw4_knn_train.dat',sep=' ',header=None)
-Test = pd.read_csv('Data/hw4_knn_test.dat',sep=' ',header=None)
-print('Data loaded. Start predicting...')
 
-start = time.clock()
+def main():
+    train_data = pd.read_csv('Data/hw4_knn_train.dat', sep=' ', header=None)
+    test_data = pd.read_csv('Data/hw4_knn_test.dat', sep=' ', header=None)
+    print('Data loaded. Start predicting...\n')
 
-K = 5
-one_neighbor = kNN(K)
-one_neighbor.train(Train[list(range(9))].values,Train[9].values)
-print('Using k-nearest-neighbor with k = %d'%K)
+    start = time.clock()
 
-Predict = one_neighbor.predict(Train[list(range(9))].values)
-Ein = sum(Predict!=Train[9].values)/len(Predict)
-print('Error rate on training set: %.2f %%'%(100*Ein))
+    K = 5
+    one_neighbor = kNN(K)
+    one_neighbor.train(train_data[list(range(9))].values, train_data[9].values)
+    print('Using k-nearest-neighbor with k = %d' % K)
 
-Predict = one_neighbor.predict(Test[list(range(9))].values)
-Eout = sum(Predict!=Test[9].values)/len(Predict)
-print('Error rate on test set: %.2f %%'%(100*Eout))
-print('Using %.2f seconds'%(time.clock()-start))
+    train_pred = one_neighbor.predict(train_data[list(range(9))].values)
+    Ein = sum(train_pred != train_data[9].values) / len(train_pred)
+    print('\tError rate on training set: %.2f %%' % (100 * Ein))
+
+    test_pred = one_neighbor.predict(test_data[list(range(9))].values)
+    Eout = sum(test_pred != test_data[9].values) / len(test_pred)
+    print('\tError rate on test set: %.2f %%' % (100 * Eout))
+
+    print('\nUsing %.2f seconds' % (time.clock() - start))
+
+
+if __name__ == '__main__':
+    main()
